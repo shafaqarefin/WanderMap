9ebc97fbfc0631f384f921baed125cab422628c3
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-04-19 04:03:23 +0600

    added nav links to respective links

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,6 +1,22 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 function AppNav() {
-  return <div> App</div>;
+  return (
+    <nav className={styles.nav}>
+      {" "}
+      <ul>
+        <NavLink to="cities">
+          {" "}
+          <li>Cities</li>
+        </NavLink>
+
+        <NavLink to="countries">
+          {" "}
+          <li>Countries</li>
+        </NavLink>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
