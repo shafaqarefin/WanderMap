8d545b9679a64f612154bb94b09c94f83536e7c4
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-04-19 23:41:55 +0600

    new use params to get id and position from url

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,6 @@
 import styles from "./City.module.css";
 import { formatDate } from "../utils/formatDate";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function City() {
   // TEMP DATA
@@ -11,41 +12,48 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.city}>
-      <div className={styles.row}>
-        <h6>City name</h6>
-        <h3>
-          <span>{emoji}</span> {cityName}
-        </h3>
-      </div>
+    // <div className={styles.city}>
+    //   <div className={styles.row}>
+    //     <h6>City name</h6>
+    //     <h3>
+    //       <span>{emoji}</span> {cityName}
+    //     </h3>
+    //   </div>
 
-      <div className={styles.row}>
-        <h6>You went to {cityName} on</h6>
-        <p>{formatDate(date || null)}</p>
-      </div>
+    //   <div className={styles.row}>
+    //     <h6>You went to {cityName} on</h6>
+    //     <p>{formatDate(date || null)}</p>
+    //   </div>
 
-      {notes && (
-        <div className={styles.row}>
-          <h6>Your notes</h6>
-          <p>{notes}</p>
-        </div>
-      )}
+    //   {notes && (
+    //     <div className={styles.row}>
+    //       <h6>Your notes</h6>
+    //       <p>{notes}</p>
+    //     </div>
+    //   )}
 
-      <div className={styles.row}>
-        <h6>Learn more</h6>
-        <a
-          href={`https://en.wikipedia.org/wiki/${cityName}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Check out {cityName} on Wikipedia &rarr;
-        </a>
-      </div>
+    //   <div className={styles.row}>
+    //     <h6>Learn more</h6>
+    //     <a
+    //       href={`https://en.wikipedia.org/wiki/${cityName}`}
+    //       target="_blank"
+    //       rel="noreferrer"
+    //     >
+    //       Check out {cityName} on Wikipedia &rarr;
+    //     </a>
+    //   </div>
 
-      <div>{/* <ButtonBack /> */}</div>
-    </div>
+    //   <div>{/* <ButtonBack /> */}</div>
+    // </div>
+    <p>
+      city {id} {lat} {lng}
+    </p>
   );
 }
 
