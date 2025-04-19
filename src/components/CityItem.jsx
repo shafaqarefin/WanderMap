7216ba84617d173c9b0a978e653e55c7663b235f
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-04-19 23:42:44 +0600

    Added Link element of router with id and position being appended to the url when clicked

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,14 +1,20 @@
 import styles from "./CityItem.module.css";
 import { formatDate } from "../utils/formatDate";
+import { Link } from "react-router-dom";
 function CityItem({ city }) {
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
 
   return (
-    <li className={styles.cityItem} key={id}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li key={id}>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={styles.cityItem}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
