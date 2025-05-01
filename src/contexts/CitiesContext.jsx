e0dec3dd31986b2fd701af6205b4c0b89cb71be3
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-02 03:02:18 +0600

    createNewCity function which allows to add new city to server api via post method

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,7 +7,6 @@ function CitiesProvider({ children }) {
   const [isLoading, setisLoading] = useState(false);
   const [cities, setCities] = useState([]);
   const [currentCity, setCurrentCity] = useState({});
-  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   useEffect(function () {
     async function fetchCities() {
@@ -37,13 +36,34 @@ function CitiesProvider({ children }) {
       setisLoading(false);
     }
   }
+
+  async function createNewCity(newCity) {
+    try {
+      setisLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setCities((cities) => [...cities, data]);
+    } catch (error) {
+      alert(`Error: ${error}`);
+    } finally {
+      setisLoading(false);
+    }
+  }
   return (
     <CitiesContext.Provider
       value={{
         isLoading,
+        setisLoading,
         cities,
         getCityById,
         currentCity,
+        createNewCity,
       }}
     >
       {children}
