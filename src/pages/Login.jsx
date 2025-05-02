b05bc109e7afd7fe15adf1adf92b743ba64b1bd2
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-03 01:21:35 +0600

    Added login functionality with authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
+import Button from "../components/Button";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmitLoginForm(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
     <main className={styles.login}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmitLoginForm}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -29,7 +46,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type={"primary"}>Login</Button>
         </div>
       </form>
     </main>
