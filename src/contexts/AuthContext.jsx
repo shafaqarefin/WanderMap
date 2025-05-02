ec0101f918326b970ea81628cc8dc492a03c6471
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-05-02 23:15:40 +0600

    created new Authenticator context that provides login and logout services

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useReducer } from "react";
+const iniitalState = { user: "", isAuthenticated: false };
+const AuthContext = createContext();
+const DUMMY_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    default:
+      throw new Error("Unknown Action");
+    case "login":
+      return { ...state, isAuthenticated: true, user: action.payload };
+
+    case "logout":
+      return { ...state, isAuthenticated: false, user: "" };
+  }
+}
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    iniitalState
+  );
+
+  function login(email, password) {
+    if (email === DUMMY_USER.email && password === DUMMY_USER.password) {
+      dispatch({ type: "login", payload: DUMMY_USER });
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+  return (
+    <AuthContext.Provider value={(login, logout, user, isAuthenticated)}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used outside Auth Provider Scope");
+  return context;
+}
+export { AuthProvider, useAuth };
