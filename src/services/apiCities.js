efcca04756feb7c3f2663c27149e6d1f5e32f0af
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-13 17:23:09 +0600

    added delete and fetching specific city api

--- src/services/apiCities.js
+++ src/services/apiCities.js
@@ -19,7 +19,22 @@ export async function createCity(newCity) {
   return data;
 }
 
-export async function deleteCity(id) {
+export async function deleteCityApi(id) {
   const { error } = await supabase.from("cities").delete().eq("id", id);
   if (error) throw error;
+}
+
+export async function fetchCityById(cityId) {
+  const { data: city, error } = await supabase
+    .from("cities")
+    .select("*")
+    .eq("id", cityId)
+    .single();
+
+  if (error) {
+    console.error("Error fetching city:", error);
+    return null;
+  }
+
+  return city;
 }
