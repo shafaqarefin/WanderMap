93f65f83c347dc2a20a55cc13030c6fe98038e48
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-13 10:53:55 +0600

    minor changes

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -14,7 +14,7 @@ export default function Product() {
           alt="person with dog overlooking mountain with sunset"
         />
         <div>
-          <h2>About WorldWide.</h2>
+          <h2>About WanderMap.</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
             dicta illum vero culpa cum quaerat architecto sapiente eius non
